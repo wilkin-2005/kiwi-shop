b7470fa94a7f9d2bf7cd778a2d96f6e6530da13c
wilkin2005 <279715150+wilkin-2005@users.noreply.github.com>
2026-09-10 11:24:47 +0200

    • Add CSS-files for footer and pagination components. • Add styling to footer and pagination components. • Import CSS-files to and edit footer and pagination components to work with new styling.

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,10 +1,17 @@
 
+import "./footer.css";
+
+
 // Footer component for the Kiwi Shop website
 export default function Footer()
 {
   return (
-    <footer>
-      <p>&copy; {new Date().getFullYear()} Kiwi Shop. Skolprojekt byggt med Next.js.</p>
+    <footer className="footer">
+      <div className="footer-container">
+
+        <p className="footer-text"> &copy; 2026 Kiwi Shop. Skolprojekt byggt med Next.js. </p>
+        {/* {new Date().getFullYear()} */}
+      </div>
     </footer>
   );
 }
--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,26 +1,46 @@
 
+import "./pagination.css";
+
 // Pagination component for the Kiwi Shop website.
 export default function Pagination()
 {
   return (
     <nav aria-label="Paginering av produkter" className="pagination-nav">
-      
       <ul className="pagination-list">
-        <li> <button type="button" disabled aria-label="Föregående sida"> &lt; </button> </li>
-        
-        <li> <button type="button" aria-current="page"> 1 </button> </li>
 
-        <li> <button type="button">2</button> </li>
-        
-        <li> <button type="button">3</button> </li>
+        <li>
+          <button type="button" disabled aria-label="Föregående sida">
+            &lt; {/* Creates an "less-than sign" "<" */}
+          </button>
+        </li>
 
-        <li aria-hidden="true"> <span>...</span> </li>
+        <li>
+          <button type="button" aria-current="page"> 1 </button>
+        </li>
 
-        <li> <button type="button">25</button> </li>
+        <li>
+          <button type="button"> 2 </button>
+        </li>
 
-        <li> <button type="button" aria-label="Nästa sida"> &gt; </button> </li>
+        <li>
+          <button type="button"> 3 </button>
+        </li>
+
+        <li aria-hidden="true">
+          <span> ... </span>
+        </li>
+
+        <li>
+          <button type="button"> 25 </button>
+        </li>
+
+        <li>
+          <button type="button" aria-label="Nästa sida">
+            &gt; {/* Creates an "greater-than sign" ">" */}
+          </button>
+        </li>
+
       </ul>
-
     </nav>
   );
 }
